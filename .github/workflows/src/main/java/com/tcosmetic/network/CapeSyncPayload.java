8c0eab06735d437ca36ae08d832b3f6fa720b2f3
Twicefear <ytd82774@gmail.com>
2026-01-09 11:54:42 +0530

    AI: Checking the content of CapeSyncPayload.java to see if there are any obvious errors.

---
+++ .github/workflows/src/main/java/com/tcosmetic/network/CapeSyncPayload.java
@@ -0,0 +1,25 @@
+package com.tcosmetic.network;
+
+import net.minecraft.network.RegistryByteBuf;
+import net.minecraft.network.codec.PacketCodec;
+import net.minecraft.network.codec.PacketCodecs;
+import net.minecraft.network.packet.CustomPayload;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.Uuids;
+
+import java.util.UUID;
+
+public record CapeSyncPayload(UUID playerUuid, String capeUrl) implements CustomPayload {
+    public static final Id<CapeSyncPayload> ID = new Id<>(Identifier.of("tcosmetic", "cape_sync"));
+
+    public static final PacketCodec<RegistryByteBuf, CapeSyncPayload> CODEC = PacketCodec.tuple(
+            Uuids.PACKET_CODEC, CapeSyncPayload::playerUuid,
+            PacketCodecs.STRING, CapeSyncPayload::capeUrl,
+            CapeSyncPayload::new
+    );
+
+    @Override
+    public Id<? extends CustomPayload> getId() {
+        return ID;
+    }
+}
