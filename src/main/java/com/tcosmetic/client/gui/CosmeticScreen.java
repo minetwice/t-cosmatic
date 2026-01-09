c6b942bdb0cb3add34e063b27c37c65976d72a4a
Twicefear <ytd82774@gmail.com>
2026-01-09 09:20:42 +0530

    AI Auto-Commit: Improved CosmeticScreen to use InventoryScreen.drawEntity for the doll.

--- src/main/java/com/tcosmetic/client/gui/CosmeticScreen.java
+++ src/main/java/com/tcosmetic/client/gui/CosmeticScreen.java
@@ -7,18 +7,10 @@ import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.ingame.InventoryScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
-import net.minecraft.client.render.DiffuseLighting;
-import net.minecraft.client.render.entity.EntityRenderDispatcher;
-import net.minecraft.client.render.entity.GeneralEntityModel;
-import net.minecraft.client.render.entity.PlayerEntityRenderer;
-import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
-import org.joml.Matrix4f;
-import org.joml.Quaternionf;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,8 +19,6 @@ import java.util.Map;
 public class CosmeticScreen extends Screen {
     private final Screen parent;
     private String tempSelectedCape = CapeManager.selectedCape;
-    private float mouseX;
-    private float mouseY;
 
     public CosmeticScreen(Screen parent) {
         super(Text.literal("TCosmetic"));
@@ -75,80 +65,25 @@ public class CosmeticScreen extends Screen {
     public void render(DrawContext context, int mouseX, int mouseY, float delta) {
         this.renderBackground(context, mouseX, mouseY, delta);
         super.render(context, mouseX, mouseY, delta);
-        
-        this.mouseX = (float)mouseX;
-        this.mouseY = (float)mouseY;
 
         // Draw Player Preview (Doll)
         int dollX = this.width / 2 + 50;
         int dollY = this.height / 2 + 50;
-        drawEntity(context, dollX, dollY, 80, (float)(dollX) - mouseX, (float)(dollY - 120) - mouseY, this.client.player);
+        
+        if (this.client.player != null) {
+            // Use InventoryScreen's static method if available, or just render nothing if null
+            // In 1.21, InventoryScreen.drawEntity exists.
+            InventoryScreen.drawEntity(context, dollX, dollY, 30, (float)(dollX) - mouseX, (float)(dollY - 50) - mouseY, this.client.player);
+        }
         
         // Draw Cape Preview Icon if selected
         if (tempSelectedCape != null) {
             Identifier capeId = CapeManager.getLoadedCapes().get(tempSelectedCape);
             if (capeId != null) {
+                // Draw texture 64x32
                 context.drawTexture(capeId, this.width / 2 + 10, 50, 0, 0, 64, 32, 64, 32);
                 context.drawText(this.textRenderer, "Selected: " + tempSelectedCape, this.width / 2 + 10, 90, 0xFFFFFF, true);
             }
         }
-    }
-
-    // Adapted from InventoryScreen
-    public static void drawEntity(DrawContext context, int x, int y, int size, float mouseX, float mouseY, LivingEntity entity) {
-        if (entity == null) return;
-        float f = (float)Math.atan((double)(mouseX / 40.0F));
-        float g = (float)Math.atan((double)(mouseY / 40.0F));
-        Quaternionf quaternionf = (new Quaternionf()).rotateZ(3.1415927F);
-        Quaternionf quaternionf2 = (new Quaternionf()).rotateX(g * 20.0F * 0.017453292F);
-        quaternionf.mul(quaternionf2);
-        float h = entity.bodyYaw;
-        float i = entity.getYaw();
-        float j = entity.getPitch();
-        float k = entity.prevHeadYaw;
-        float l = entity.headYaw;
-        entity.bodyYaw = 180.0F + f * 20.0F;
-        entity.setYaw(180.0F + f * 40.0F);
-        entity.setPitch(-g * 20.0F);
-        entity.headYaw = entity.getYaw();
-        entity.prevHeadYaw = entity.getYaw();
-        
-        // Backup cape
-        String originalCape = CapeManager.selectedCape;
-        // Temporarily set the cape to what we are previewing so the renderer picks it up
-        if (entity instanceof PlayerEntity) {
-             // This is a bit hacky because the renderer reads from CapeManager directly.
-             // Ideally we'd pass the texture to the renderer, but for this simple mod, 
-             // we rely on the global state or mixin interception.
-             // Since we are just rendering a preview, we can't easily inject the texture without more complex mixins.
-             // For now, the preview might show the CURRENTLY EQUIPPED cape, not the temp one, 
-             // unless we modify CapeManager to support overrides or the Mixin to check a "preview" state.
-        }
-
-        Vector3f translation = new Vector3f();
-        // Matrix transformations... 
-        // Simpler approach: Use standard InventoryScreen logic
-        
-        context.getMatrices().push();
-        context.getMatrices().translate((float)x, (float)y, 1050.0F);
-        context.getMatrices().scale(1.0F, 1.0F, -1.0F);
-        RenderSystem.applyModelViewMatrix();
-        Matrix4f matrix4f = context.getMatrices().peek().getPositionMatrix();
-        
-        // ... (Complex rendering logic omitted for brevity, using simplified call if possible or just text if too complex for raw code)
-        // Actually, let's try to keep it simple.
-        
-        // Since implementing full entity rendering from scratch is error-prone in this format,
-        // we will rely on standard methods if available or simple placeholders.
-        // However, InventoryScreen.drawEntity is standard.
-        // We need to implement it fully or call it if accessible. It is static in InventoryScreen usually.
-        // In 1.21 it might have moved or changed.
-        
-        // Let's assume we can't easily replicate the perfect doll code without imports.
-        // I will put a placeholder text "Preview" instead of potentially broken GL code.
-        
-        context.drawText(MinecraftClient.getInstance().textRenderer, "Preview Area", x - 20, y - 50, 0xFFFFFF, true);
-        
-        context.getMatrices().pop();
     }
 }
